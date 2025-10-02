15d666510d8bbda465632a83bce40a44968034db
Abigail0311 <abigail.rev.seg@gmail.com>
2025-10-02 18:35:09 -0500

    Add files via upload

--- MundoHumano.cpp
+++ MundoHumano.cpp
@@ -1,1 +1,13 @@
 #include "MundoHumano.h"
+
+using namespace std;
+
+int main() {
+    vector<MundoHumano*> humanos;
+    humanos.push_back(new EnemigoHumano());
+    humanos.push_back(new PreguntaHumano());
+    humanos.push_back(new ProtagonistaHumano());
+    humanos.push_back(new RecursoHumano());
+
+    return 0;
+}
--- MundoHumano.h
+++ MundoHumano.h
@@ -1,6 +1,53 @@
 #pragma once
 #include "Mundo.h"
-class MundoHumano :public Mundo
-{
+#include <iostream>
+#include <string>
+#include <vector>
+
+class MundoHumano : public Mundo {
+private:
+    std::string nombre;
+    Protagonista protagonista;
+    Enemigo enemigo;
+    Recurso recurso;
+    Pregunta pregunta;
+
+public:
+    virtual void iniciarMundo();
+    virtual bool verificarCondiciones();
 };
 
+class EnemigoHumano : public MundoHumano {
+private:
+    int daño;
+public:
+    void mover();
+    void recibirDaño(int);
+};
+
+class PreguntaHumano : public MundoHumano {
+private:
+    std::string enunciado;
+    std::string opciones;
+    std::string respuestaCorrecta;
+public:
+    void mostrarPregunta();
+    bool verificarRespuesta(std::string);
+};
+
+class ProtagonistaHumano : public MundoHumano {
+private:
+    int energia;
+public:
+    void recolectarRecurso();
+    bool responderPregunta();
+};
+
+class RecursoHumano : public MundoHumano {
+private:
+    std::string tipo;
+    int rx;
+    int ry;
+public:
+    void activar(ProtagonistaHumano);
+};
