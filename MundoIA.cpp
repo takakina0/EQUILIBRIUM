09348d167722acdabd302efc065931ad648b032a
Matdcr1 <leomatdcr311@gmail.com>
2025-10-02 17:45:01 -0500

    Add files via upload

--- MundoIA.cpp
+++ MundoIA.cpp
@@ -1,1 +1,13 @@
 #include "MundoIA.h"
+
+using namespace std;
+
+int main() {
+	vector<MundoIA*>IA;
+	IA.push_back(new EnemigoIA());
+	IA.push_back(new PreguntaIA());
+	IA.push_back(new Protagonista());
+	IA.push_back(new Recurso());
+
+	return 0;
+}
--- MundoIA.h
+++ MundoIA.h
@@ -1,6 +1,52 @@
 #pragma once
 #include "Mundo.h"
-class MundoIA :public Mundo
-{
+#include <iostream>
+#include <string>
+#include <vector>
+
+class MundoIA :public Mundo {
+private:
+	std::string nombre;
+	Protagonista protagonista;
+	Enemigo enemigo;
+	Recurso recurso;
+	Pregunta pregunta;
+public:
+	virtual void iniciarMundo();
+	virtual bool verificarCondiciones();
 };
 
+class EnemigoIA : public MundoIA {
+private:
+	int daño;
+public:
+	void mover();
+	void recibirDaño(int);
+};
+
+class PreguntaIA : public MundoIA {
+private:
+	std::string enunciado;
+	std::string opciones;
+	std::string respuestaCorrecta;
+public:
+	void mostrarPregunta();
+	bool verificarRespuesta(std::string);
+};
+
+class Protagonista : public MundoIA {
+private:
+	int energia;
+public:
+	void recolectarRecurso();
+	bool responderPregunta();
+};
+
+class Recurso : MundoIA {
+private:
+	std::string tipo;
+	int rx;
+	int ry;
+public:
+	void activar(Protagonista);
+};
